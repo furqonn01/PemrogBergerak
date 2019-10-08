dd81768af853ecd717d501b85ac0c9b153aba8f4
furqonn01 <frqnnugroho@gmail.com>
2019-10-09 04:17:03 +0700

    Tes

---
+++ DonasiPesawat/MyViewAndViews/app/src/main/java/com/example/myviewandviews/DonasiActivity.java
@@ -0,0 +1,17 @@
+package com.example.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donasi);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Donasi");
+        }
+    }
+}
