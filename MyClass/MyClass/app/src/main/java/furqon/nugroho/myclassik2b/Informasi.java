dd361e3f9039ceb01680aa7dfdb9c3feaec1e882
furqonn01 <frqnnugroho@gmail.com>
2020-01-04 22:42:39 +0700

    Class

---
+++ MyClass/MyClass/app/src/main/java/furqon/nugroho/myclassik2b/Informasi.java
@@ -0,0 +1,14 @@
+package furqon.nugroho.myclassik2b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Informasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informasi);
+    }
+}
